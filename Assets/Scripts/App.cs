67783086c22b53ca47b154ddfd25086d12614496
Shin Ishimaru <shinishimaru@gmail.com>
2016-06-07 15:56:29 +0900

    first commit

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using CsvHelper;
+
+public class App : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		TextAsset csv = Resources.Load( "sample" ) as TextAsset;
+
+		StringReader    strReader = new StringReader(csv.text);
+		CsvReader       csvReader = new CsvReader(strReader);
+
+		while (csvReader.Read())
+		{
+			string[] records = csvReader.CurrentRecord;
+			Debug.Log(records[0]);
+			Debug.Log(records[1]);
+			Debug.Log(records[2]);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
